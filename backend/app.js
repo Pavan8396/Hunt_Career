216cdd424039e0fddcbae4964004f01fb269f963
Pavan <pavanumashankar8@gmail.com>
2025-10-28 19:02:58 +0530

    app.js update

--- backend/app.js
+++ backend/app.js
@@ -46,7 +46,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/api/auth', authRoutes);
 app.use('/api/jobs', jobRoutes);
 app.use('/api/user', userRoutes);
-app.use('/api/employer', employerRoutes);
+app.use('/api/employers', employerRoutes);
 app.use('/api/applications', applicationRoutes);
 app.use('/api/chat', chatRoutes);
 
